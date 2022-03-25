4d1cf4cb734f820daf911e4e6d8de393ddaeb657
Garry Newman <garrynewman@users.noreply.github.com>
2022-03-25 14:16:50 +0000

    Crowbar, Python

---
+++ code/Entities/Weapons/Crowbar.cs
@@ -0,0 +1,56 @@
+﻿[Library( "dm_crowbar", Title = "Crowbar" )]
+[Hammer.EditorModel( "weapons/rust_pistol/rust_pistol.vmdl" )]
+partial class Crowbar : DeathmatchWeapon
+{
+	public override string ViewModelPath => "weapons/rust_pistol/v_rust_pistol.vmdl";
+
+	public override float PrimaryRate => 2.0f;
+	public override float SecondaryRate => 1.0f;
+	public override float ReloadTime => 3.0f;
+	public override AmmoType AmmoType => base.AmmoType;
+	public override int ClipSize => 0;
+	public override int Bucket => 0;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "weapons/rust_pistol/rust_pistol.vmdl" );
+		AmmoClip = 0;
+	}
+
+	public override bool CanPrimaryAttack()
+	{
+		return base.CanPrimaryAttack();
+	}
+
+	public override void AttackPrimary()
+	{
+		TimeSincePrimaryAttack = 0;
+		TimeSinceSecondaryAttack = 0;
+
+		// woosh sound
+		// screen shake
+
+		Rand.SetSeed( Time.Tick );
+
+		var forward = Owner.EyeRotation.Forward;
+		forward += (Vector3.Random + Vector3.Random + Vector3.Random + Vector3.Random) * 0.1f;
+		forward = forward.Normal;
+
+		foreach ( var tr in TraceBullet( Owner.EyePosition, Owner.EyePosition + forward * 70, 5 ) )
+		{
+			tr.Surface.DoBulletImpact( tr );
+
+			if ( !IsServer ) continue;
+			if ( !tr.Entity.IsValid() ) continue;
+
+			var damageInfo = DamageInfo.FromBullet( tr.EndPosition, forward * 100 * 1, 5 )
+				.UsingTraceResult( tr )
+				.WithAttacker( Owner )
+				.WithWeapon( this );
+
+			tr.Entity.TakeDamage( damageInfo );
+		}
+	}
+}
---
+++ code/Entities/Weapons/Python.cs
@@ -0,0 +1,56 @@
+﻿[Library( "dm_357", Title = ".357 Magnum Revolver" )]
+[Hammer.EditorModel( "weapons/rust_pistol/rust_pistol.vmdl" )]
+partial class Python : DeathmatchWeapon
+{
+	public override string ViewModelPath => "weapons/rust_pistol/v_rust_pistol.vmdl";
+
+	public override float PrimaryRate => 2.0f;
+	public override float SecondaryRate => 1.0f;
+	public override float ReloadTime => 7.0f;
+	public override int ClipSize => 6;
+	public override AmmoType AmmoType => AmmoType.Python;
+
+	public override int Bucket => 1;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "weapons/rust_pistol/rust_pistol.vmdl" );
+		AmmoClip = 6;
+	}
+
+	public override bool CanPrimaryAttack()
+	{
+		return base.CanPrimaryAttack() && Input.Pressed( InputButton.Attack1 );
+	}
+
+	public override void AttackPrimary()
+	{
+		TimeSincePrimaryAttack = 0;
+		TimeSinceSecondaryAttack = 0;
+
+		if ( !TakeAmmo( 1 ) )
+		{
+			DryFire();
+			return;
+		}
+
+		//
+		// Tell the clients to play the shoot effects
+		//
+		ShootEffects();
+		PlaySound( "rust_pistol.shoot" );
+
+		//
+		// Shoot the bullets
+		//
+		ShootBullet( 0.2f, 1.5f, 40.0f, 3.0f );
+
+		if ( IsClient && IsLocalPawn )
+		{
+			new Sandbox.ScreenShake.Perlin( 2, 2, 3, 0 );
+		}
+
+	}
+}
