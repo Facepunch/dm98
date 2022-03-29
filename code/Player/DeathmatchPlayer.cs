efa007f44b2692f80e23e8b47908b1435b7d8c8d
Garry Newman <garrynewman@users.noreply.github.com>
2022-03-29 12:49:00 +0100

    Remove unused code

--- code/Player/DeathmatchPlayer.cs
+++ code/Player/DeathmatchPlayer.cs
@@ -83,9 +83,6 @@
 
 	public override void Simulate( Client cl )
 	{
-		//if ( cl.NetworkIdent == 1 )
-		//	return;
-
 		base.Simulate( cl );
 
 		//
@@ -159,33 +156,11 @@
 		base.StartTouch( other );
 	}
 
-	Rotation lastCameraRot = Rotation.Identity;
-
 	public override void PostCameraSetup( ref CameraSetup setup )
 	{
 		setup.ZNear = 0.1f;
 
 		base.PostCameraSetup( ref setup );
-
-		if ( lastCameraRot == Rotation.Identity )
-			lastCameraRot = setup.Rotation;
-
-		var angleDiff = Rotation.Difference( lastCameraRot, setup.Rotation );
-		var angleDiffDegrees = angleDiff.Angle();
-		var allowance = 20.0f;
-
-		if ( angleDiffDegrees > allowance )
-		{
-			// We could have a function that clamps a rotation to within x degrees of another rotation?
-			lastCameraRot = Rotation.Lerp( lastCameraRot, setup.Rotation, 1.0f - (allowance / angleDiffDegrees) );
-		}
-		else
-		{
-			//lastCameraRot = Rotation.Lerp( lastCameraRot, Camera.Rotation, Time.Delta * 0.2f * angleDiffDegrees );
-		}
-
-		// uncomment for lazy cam
-		//camera.Rotation = lastCameraRot;
 
 		if ( setup.Viewer != null )
 		{
@@ -214,23 +189,17 @@
 		setup.Position += left * MathF.Sin( walkBob * 0.6f ) * speed * 1;
 
 		// Camera lean
-		lean = lean.LerpTo( Velocity.Dot( setup.Rotation.Right ) * 0.03f, Time.Delta * 15.0f );
+		lean = lean.LerpTo( Velocity.Dot( setup.Rotation.Right ) * 0.01f, Time.Delta * 15.0f );
 
 		var appliedLean = lean;
-		appliedLean += MathF.Sin( walkBob ) * speed * 0.2f;
+		appliedLean += MathF.Sin( walkBob ) * speed * 0.3f;
 		setup.Rotation *= Rotation.From( 0, 0, appliedLean );
 
 		speed = (speed - 0.7f).Clamp( 0, 1 ) * 3.0f;
 
-		fov = fov.LerpTo( speed * 20 * MathF.Abs( forwardspeed ), Time.Delta * 2.0f );
+		fov = fov.LerpTo( speed * 20 * MathF.Abs( forwardspeed ), Time.Delta * 4.0f );
 
 		setup.FieldOfView += fov;
-
-		//	var tx = new Sandbox.UI.PanelTransform();
-		//	tx.AddRotation( 0, 0, lean * -0.1f );
-
-		//	Hud.CurrentPanel.Style.Transform = tx;
-		//	Hud.CurrentPanel.Style.Dirty(); 
 
 	}
 
