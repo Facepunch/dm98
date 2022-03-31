ed57f266721d9ed2efb745efc5add141f71dc5d1
Garry Newman <garrynewman@users.noreply.github.com>
2022-03-31 15:17:48 +0100

    Footstep boost

--- code/Player/DeathmatchPlayer.cs
+++ code/Player/DeathmatchPlayer.cs
@@ -340,4 +340,34 @@
 			ply.TookDamage( ply.Position + ply.EyeRotation.Forward * 100.0f );
 		}
 	}
+
+	TimeSince timeSinceLastFootstep = 0;
+
+	public override void OnAnimEventFootstep( Vector3 pos, int foot, float volume )
+	{
+		if ( LifeState != LifeState.Alive )
+			return;
+
+		if ( !IsServer )
+			return;
+
+		if ( timeSinceLastFootstep < 0.2f )
+			return;
+
+		volume *= FootstepVolume();
+
+		timeSinceLastFootstep = 0;
+
+		//DebugOverlay.Box( 1, pos, -1, 1, Color.Red );
+		//DebugOverlay.Text( pos, $"{volume}", Color.White, 5 );
+
+		var tr = Trace.Ray( pos, pos + Vector3.Down * 20 )
+			.Radius( 1 )
+			.Ignore( this )
+			.Run();
+
+		if ( !tr.Hit ) return;
+
+		tr.Surface.DoFootstep( this, tr, foot, volume * 10 );
+	}
 }
